90d50b2c60489a4491f6bc911ac90d72608d1d10
Azret Zhusuev <a.zhusuev@quickpay.kg>
2023-05-25 11:02:58 +0600

    add routes

--- routes/api.ts
+++ routes/api.ts
@@ -1,8 +1,26 @@
 import express, { Request, Response } from 'express';
 import { AbonentServiceController } from '../controllers/AbonentServiceController';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import { UserController } from '../controllers/UserController';
+import { AuthMiddleware } from '../middleware/AuthMiddleware';
+import cors from 'cors';
+
+// Enable CORS for all routes
+dotenv.config();
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/login', UserController.login);
+app.get('/users', AuthMiddleware.authenticate, UserController.getUsers);
+
+app.get('/', (req, res) => {
+  res.send('Express + TypeScript Server');
+});
 
 app.get('/api/listServices', AbonentServiceController.getServices);
 app.get('/api/properties/:id', AbonentServiceController.getProperties);
