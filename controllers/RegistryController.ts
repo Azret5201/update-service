44d02b32c901f5e1c94c0f5d6279aadbbe08113d
iskanderomanov <iskanderomanov@gmail.com>
2023-06-26 12:51:19 +0600

    rename registry

--- controllers/RegistryController.ts
+++ controllers/RegistryController.ts
@@ -14,7 +14,7 @@ export class RegistryServiceController {
   }
 
   public async store(req: Request, res: Response): Promise<void> {
-    if (!req.body.file && !req.body.serviceId && !req.body.identifierOrder) {
+    if (!req.body.file && !req.body.serviceId && !req.body.serverId) {
       res.status(400).json({ error: "don't have required paramerts" });
     }
 
@@ -23,14 +23,21 @@ export class RegistryServiceController {
         if (columnOrder) {
           // Запись найдена, выполняем обновление
           return columnOrder.update({
-            identifier_order: req.body.identifierOrder,
+            serverId: req.body.serverId,
+            title: req.body.title,
+            tableHeaders: req.body.tableHeaders,
+            fields: req.body.fields,
+            sql: req.body.sql,
             pay_sum_order: req.body.paySumOrder,
           });
         } else {
           // Запись не найдена, создаем новую
           return ColumnOrder.create({
-            service_id: req.body.serviceId,
-            identifier_order: req.body.identifierOrder,
+            serverId: req.body.serverId,
+            title: req.body.title,
+            tableHeaders: req.body.tableHeaders,
+            fields: req.body.fields,
+            sql: req.body.sql,
             pay_sum_order: req.body.paySumOrder,
           });
         }
@@ -41,20 +48,5 @@ export class RegistryServiceController {
       .catch((error) => {
         res.status(500).json("Upsert operation failed:" + error);
       });
-
-    const outputFilePath = __dirname + "/../storage/" + req.body.serviceId + new Date().getTime().toString() + ".csv";
-
-    const base64Content = req.body.file.split(";base64,").pop();
-    const fileStream = createWriteStream(outputFilePath);
-    fileStream.write(Buffer.from(base64Content, "base64"));
-    fileStream.end();
-
-    fileStream.on("finish", () => {
-      res.status(200).json({ response: "Successful" });
-    });
-
-    fileStream.on("error", (err) => {
-      res.status(500).json({ error: "Not save file!" });
-    });
   }
 }
