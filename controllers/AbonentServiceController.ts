47b391e8e175d7b3c7818e2a03ca27fd277c9fd2
Azret Zhusuev <a.zhusuev@quickpay.kg>
2023-10-09 16:33:53 +0600

    fx bags

--- controllers/AbonentServiceController.ts
+++ controllers/AbonentServiceController.ts
@@ -51,34 +51,29 @@ export class AbonentServiceController {
     let fileData = '';
     if (format !== 'text/csv') {
       try {
-        if (mimeTypes.extension(format) === 'xlsx') {
-
           fileData = AbonentServiceController.convertXLSX(splitted[1]);
-        } else {
-          // AbonentServiceController.convertDBF(splitted[1]);
-        }
       } catch (error) {
         console.error('Error converting base64 to CSV:', error);
       throw error;
       }
     } 
 
-    // const data = typeof fileData === 'string' ? fileData :  Buffer.from(splitted[1], "base64");
+    const data = typeof fileData === 'string' ? fileData :  Buffer.from(splitted[1], "base64");
     
-    // const outputFilePath = __dirname + "/../storage/" + req.body.serviceId + "_" + new Date().getTime().toString() + ".csv";
+    const outputFilePath = __dirname + "/../storage/" + req.body.serviceId + "_" + new Date().getTime().toString() + ".csv";
 
-    // const fileStream = createWriteStream(outputFilePath);
-    // fileStream.write(data);
-    // fileStream.end();
+    const fileStream = createWriteStream(outputFilePath);
+    fileStream.write(data);
+    fileStream.end();
 
 
-    // fileStream.on("finish", () => {
-    //   res.status(200).json({ response: "Successful" });
-    // });
+    fileStream.on("finish", () => {
+      res.status(200).json({ response: "Successful" });
+    });
 
-    // fileStream.on("error", (err) => {
-    //   res.status(500).json({ error: "Not save file!" });
-    // });
+    fileStream.on("error", (err) => {
+      res.status(500).json({ error: "Not save file!" });
+    });
     
   }
 
@@ -93,58 +88,4 @@ export class AbonentServiceController {
 
     return csvData;
   }
-      
-      
-      // public static async convertDBF(data: string) {
-
-      //   const dbfBuffer = Buffer.from(data, 'base64');
-      //   const csvFilePath = __dirname + '/../storage/dbfTest.csv';
-      //   const csvStream = createWriteStream(csvFilePath);
-      //   csvStream.write(data);
-
-      //   const size = 20;
-      //   const decodeDbfBuffer = iconv.decode(dbfBuffer, 'win1251');
-
-        // const typeReplacements = {
-        //   string: 'C',
-        //   numeric: 'N',
-        //   float: 'F',
-        //   integer: 'I',
-        //   date: 'D',
-        //   datetime: 'T',
-        //   double: 'B',
-        // };
-       
-
-        // // write header to dbf
-        // const datatable: DataTable = Dbf.read(dbfBuffer);
-
-        // const columnsWithSize = datatable.columns.map(column => ({
-        //    ...column, 
-        //    type: typeReplacements[column.type] || column.type,
-        //    size
-        //   }));
-        // let dbf = await DBFFile.create(csvFilePath, columnsWithSize);
-
-        // dbf.appendRecords(datatable);
-
-
-        // datatable.rows.forEach((row) => {
-          // console.log(iconv.decode(row.PAYER_NAME, 'win1251'));
-        // })
-
-      // // let dbf = await DBFFile.create('temp.dbf', fieldDescriptors);
-        // writeFileSync('temp.dbf', dbfBuffer);
-
-        // const stringFromBuffer = dbfBuffer.toString("latin1");
-        // // let dbf = await DBFFile.open(stringFromBuffer);
-        // console.log(stringFromBuffer);
-        // console.log(dbf);
-      //   let decode = new TextDecoder
-
-        
-
-
-      //   for await (let record of dbf) console.log();
-      // }
 }
