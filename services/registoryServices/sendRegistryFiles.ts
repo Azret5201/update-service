26392c73a1142cb85f39b427df68438bdec4cb06
Nikita <nikita@mancho.dev>
2023-11-28 14:28:11 +0600

    fix email bugs

--- services/registoryServices/sendRegistryFiles.ts
+++ services/registoryServices/sendRegistryFiles.ts
@@ -1,6 +1,7 @@
 import * as nodemailer from 'nodemailer';
 import * as fs from "fs";
 import {Response} from "express";
+import {logError} from "../../utils/logger";
 
 export const sendRegistryFiles = async (emailAddresses: string, registryFiles: string[]) => {
     const transporter = nodemailer.createTransport({
@@ -27,9 +28,16 @@ export const sendRegistryFiles = async (emailAddresses: string, registryFiles: s
                     content: fs.readFileSync(`files/` + filename),
                 }))
             };
-
             // Отправка письма
-            console.log(await transporter.sendMail(mailOptions));
+            try {
+                console.log(await transporter.sendMail(mailOptions));
+            } catch (error:any){
+                if (error.responseCode != 550) {
+                    throw error;
+                }else {
+                    logError(error);
+                }
+            }
         }
 
         // Вернуть успешный результат после успешной отправки всех писем
