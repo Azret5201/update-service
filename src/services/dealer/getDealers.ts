06c2d147ce3f8783fa1c92dcdcf1d11ce8d73536
Azret Zhusuev <a.zhusuev@quickpay.kg>
2024-03-28 17:15:27 +0600

    fx telegram bot script

--- src/services/dealer/getDealers.ts
+++ src/services/dealer/getDealers.ts
@@ -1,97 +1,105 @@
 import { Dealer } from "../../models/Dealer";
-import { sequelize } from '../../models';
+import { sequelize } from "../../models";
 import { copyFile } from "fs";
 import { Logger } from "../../utils/logger2";
 
-const logger = new Logger('telegram_bot');
+const logger = new Logger("telegram_bot");
+
+export const prodBot = "7037383166:AAEVorrD1oFmGfR7PRHyvmNhnBy3IVv8tS4";
+export const testBot = "6708220727:AAGfVxcx6lYVbSkEokgqrmJtCXuq4CDoUNg";
+
+export const helloMessage = "Привет! Я твой личный финансовый помощник 🤖💰\n\nЧтобы получить доступ к информации о балансе на вашем счете, сначала необходимо пройти авторизацию.\n\nДля этого просто нажмите на кнопку <b>\"Отправить номер телефона\"</b> ниже, чтобы подтвердить свою личность.\n\nПосле успешной авторизации вы сможете узнать свой текущий баланс, нажав на кнопку <b>Баланс</b>."
 
 export const getDealerByPhone = async (phone: string, userId: number) => {
-    try {
-        const sqlQuery = `SELECT id FROM regions WHERE phone = '${phone}'`;
-        const [results, metadata]: [any[], any] = await sequelize.query(sqlQuery);
+  try {
+    const sqlQuery = `SELECT id FROM regions WHERE phone = '${phone}'`;
+    const [results, metadata]: [any[], any] = await sequelize.query(sqlQuery);
 
-        if (results && results.length > 0) {
-            logger.log(`Found the dealer by phone ${userId}`);
-            setDealer(results[0].id, userId);
+    if (results && results.length > 0) {
+      logger.log(`Found the dealer by phone ${userId}`);
+      setDealer(results[0].id, userId);
 
-            return true;
-        } else {
-            logger.log(`Couldn't find such a dealer ${userId}`);
+      return true;
+    } else {
+      logger.log(`Couldn't find such a dealer ${userId}`);
 
-            return false; 
-        }
-    } catch (error) {
-        console.error('Ошибка в функции getDuosDealer:', error);
-        throw error; // Перевыбросить ошибку для ее передачи вызывающему
+      return false;
     }
-}
+  } catch (error) {
+    console.error("Ошибка в функции getDuosDealer:", error);
+    throw error; // Перевыбросить ошибку для ее передачи вызывающему
+  }
+};
 
 export const setDealer = async (dealerId: number, telegramId: number) => {
-    try {
-        logger.log(`---Adding user to bot clients----`);
-        const sqlQuery = `
+  try {
+    logger.log(`---Adding user to bot clients----`);
+    const sqlQuery = `
             INSERT INTO bot_clients (region_id, telegram_id)
             VALUES (${dealerId}, ${telegramId})
         `;
 
-        logger.log(`---Check user in bot clients by telegram id ${telegramId}----`);
-        const user = await checkUser(telegramId);
-        logger.log(`Result founding user in bo_clients: ${user}----`);
-        if (!user) {
-            logger.log(`---User ${dealerId} doesn't exist in bot clients ----\nInsert to bot_clients!`);
+    logger.log(`---Check user in bot clients by telegram id ${telegramId}----`);
+    const user = await checkUser(telegramId);
+    logger.log(`Result founding user in bo_clients: ${user}----`);
+    if (!user) {
+      logger.log(
+        `---User ${dealerId} doesn't exist in bot clients ----\nInsert to bot_clients!`
+      );
 
-            await sequelize.query(sqlQuery);
-            logger.log('Данные успешно добавлены в таблицу bot_clients');
-        }
-
-    } catch (error) {
-        logger.log(`ERROR: Ошибка при добавлении данных в таблицу bot_clients: ${error}`);
-        throw error; // Перевыбросываем ошибку для обработки выше
+      await sequelize.query(sqlQuery);
+      logger.log("Данные успешно добавлены в таблицу bot_clients");
     }
-}
+  } catch (error) {
+    logger.log(
+      `ERROR: Ошибка при добавлении данных в таблицу bot_clients: ${error}`
+    );
+    throw error; // Перевыбросываем ошибку для обработки выше
+  }
+};
 
 export const getBalance = async (telegramId: number) => {
-    const userDealerId = await getUser(telegramId);
-    let message = '';
-    if (userDealerId !== null) {
-        const dealer = await getDealerById(userDealerId);
-        if (dealer !== null) {
-            message = `Ваш баланс: ${dealer.balance} \nВаш кредит: ${dealer.credit}`
-        }
-    } else {
-        message = 'К сожалению не получилось взять ваш баланс. Обратитесь к вашему мменеджеру!'
+  const userDealerId = await getUser(telegramId);
+  let message = "";
+  if (userDealerId !== null) {
+    const dealer = await getDealerById(userDealerId);
+    if (dealer !== null) {
+      message = `Ваш баланс: ${dealer.balance} \nВаш кредит: ${dealer.credit}`;
     }
+  } else {
+    message =
+      "К сожалению не получилось взять ваш баланс. Обратитесь к вашему мменеджеру!";
+  }
 
-    return message;
-}
+  return message;
+};
 
 export const checkUser = async (telegramId: number) => {
-    logger.log('Start checking ...');
-    const sqlQuery = `SELECT * FROM bot_clients WHERE telegram_id = ${telegramId}`;
+  logger.log("Start checking ...");
+  const sqlQuery = `SELECT * FROM bot_clients WHERE telegram_id = ${telegramId}`;
 
-    const [results, metadata] = await sequelize.query(sqlQuery);
+  const [results, metadata] = await sequelize.query(sqlQuery);
 
-    return results.length > 0 ? true : false;
-}
+  return results.length > 0 ? true : false;
+};
 
 export const getUser = async (telegramId: number) => {
-    const sqlQuery = `SELECT region_id FROM bot_clients WHERE telegram_id = ${telegramId} LIMIT 1`;
+  const sqlQuery = `SELECT region_id FROM bot_clients WHERE telegram_id = ${telegramId} LIMIT 1`;
+
+  const [results, metadata]: [any[], any] = await sequelize.query(sqlQuery);
+
+  return results.length > 0 ? results[0].region_id : null;
+};
+
+export const getDealerById = async (id: number) => {
+  try {
+    const sqlQuery = `SELECT balance, credit FROM regions WHERE id = ${id}`;
 
     const [results, metadata]: [any[], any] = await sequelize.query(sqlQuery);
 
-
-    return results.length > 0 ? results[0].region_id : null;
-}
-
-export const getDealerById = async (id: number) => {
-    try {
-        const sqlQuery = `SELECT balance, credit FROM regions WHERE id = ${id}`;
-
-        const [results, metadata]: [any[], any] = await sequelize.query(sqlQuery);
-
-        return results.length > 0 ? results[0] : null;
-    } catch (error) {
-        console.error('Ошибка в функции getDuosDealer:', error);
-        throw error;
-    }
-}
+    return results.length > 0 ? results[0] : null;
+  } catch (error) {
+    console.error("Ошибка в функции getDuosDealer:", error);
+    throw error;
+  }
+};
--- src/services/telegram/bot.ts
+++ src/services/telegram/bot.ts
@@ -1,58 +1,65 @@
-import { Telegraf, Markup, session } from 'telegraf';
-import { getBalance, getDealerByPhone, retryCount, setDealer } from '../dealer/getDealers';
+import { Telegraf, Markup, session } from "telegraf";
+import { getBalance, getDealerByPhone, helloMessage, testBot } from "../dealer/getDealers";
 import * as dotenv from "dotenv";
 import { getAbsolutePath } from "../../utils/pathUtils";
-import sequelize from '../../../config/sequelize';
+import sequelize from "../../../config/sequelize";
 
-const bot = new Telegraf('6708220727:AAGfVxcx6lYVbSkEokgqrmJtCXuq4CDoUNg');
+const bot = new Telegraf(testBot);
 
 bot.start((ctx) => {
-    ctx.reply('Привет! Пожалуйста, отправьте свой номер телефона для авторизации.', {
-        reply_markup: {
-            keyboard: [
-                [{ text: "Отправить номер телефона", request_contact: true }]
-            ],
-            one_time_keyboard: true,
-            resize_keyboard: true
-        }
-    });
+  ctx.replyWithHTML(helloMessage, { parse_mode: 'HTML' });
+  ctx.reply(
+    "Привет! Пожалуйста, отправьте свой номер телефона для авторизации.",
+    {
+      reply_markup: {
+        keyboard: [
+          [{ text: "Отправить номер телефона", request_contact: true }],
+        ],
+        one_time_keyboard: true,
+        resize_keyboard: true,
+      },
+    }
+  );
 });
 
-bot.on('contact', async (ctx) => {
-    const contact = ctx.message?.contact;
+bot.on("contact", async (ctx) => {
+  const contact = ctx.message?.contact;
 
-    if (contact) {
-        const phoneNumber = contact.phone_number;
-        const userId = ctx.from?.id;
+  if (contact) {
+    const phoneNumber = contact.phone_number;
+    const userId = ctx.from?.id;
 
-        const dealer = await getDealerByPhone(phoneNumber, userId);
+    const dealer = await getDealerByPhone(phoneNumber, userId);
 
-        if (dealer) {
-            const authenticatedKeyboard = Markup.keyboard([
-                ['Баланс'],
-            ]).resize();
-            
-            ctx.reply(`Выберите действие:`);
+    if (dealer) {
+      const authenticatedKeyboard = Markup.keyboard([["Баланс"]]).resize();
 
-            ctx.reply('Вы успешно прошли авторизацию!', authenticatedKeyboard);
-        } else {
-            ctx.replyWithHTML(`Вы не прошли авторизацию!\n\nВозможно у вас неправильно введен ваш номер телефона! Обратитесь к вашему менеджеру!`);
-        }
+      ctx.reply(`Выберите действие:`);
+
+      ctx.reply("Вы успешно прошли авторизацию!", authenticatedKeyboard);
     } else {
-        ctx.reply('Что-то пошло не так. Пожалуйста, попробуйте еще раз.');
+      ctx.replyWithHTML(
+        `Вы не прошли авторизацию!\n\nВозможно у вас неправильно введен ваш номер телефона! Обратитесь к вашему менеджеру!`
+      );
     }
+  } else {
+    ctx.reply("Что-то пошло не так. Пожалуйста, попробуйте еще раз.");
+  }
 });
 
 //получить баланс
-bot.hears('Баланс', async (ctx) => {
-    const userId = ctx.from?.id; 
-    const dealer = await getBalance(userId);
+bot.hears("Баланс", async (ctx) => {
+  const userId = ctx.from?.id;
+  const dealer = await getBalance(userId);
 
-    ctx.reply(dealer);
-})
-
-bot.launch().then(() => {
-    console.log('Бот запущен');
-}).catch(err => {
-    console.error('Ошибка запуска бота:', err);
+  ctx.reply(dealer);
 });
+
+bot
+  .launch()
+  .then(() => {
+    console.log("Бот запущен");
+  })
+  .catch((err) => {
+    console.error("Ошибка запуска бота:", err);
+  });
